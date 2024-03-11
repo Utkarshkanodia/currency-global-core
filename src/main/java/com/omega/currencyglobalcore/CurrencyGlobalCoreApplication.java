6cc3e41a2546c32620e9c750192d84ce764ecca8
Utkarsh Kanodia <utkarshkanodia22@outlook.com>
2024-03-11 19:29:44 +0530

    first commit

---
+++ src/main/java/com/omega/currencyglobalcore/CurrencyGlobalCoreApplication.java
@@ -0,0 +1,13 @@
+package com.omega.currencyglobalcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyGlobalCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyGlobalCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/omega/currencyglobalcore/CurrencyGlobalCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omega.currencyglobalcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyGlobalCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
